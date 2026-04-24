4f92c1e6ccac5202daab6029d0cd9630aeb236ce
kiyang <109514563+Kiyang101@users.noreply.github.com>
2026-04-24 14:37:49 +0700

    chore: remove MacroRecorder component and entry-point route

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { PointOverlay } from "./PointOverlay";
-import { MacroRecorder } from "./MacroRecorder";
 
-const urlParams  = new URLSearchParams(window.location.search);
-const pointId    = urlParams.get("point");
-const isRecorder = urlParams.get("recorder") === "1";
-
-// Set transparent background before React renders so WebView2 on Windows
-// never paints a white frame.
-if (isRecorder) {
-  document.documentElement.style.background = "transparent";
-  document.body.style.cssText = "margin:0;background:transparent;";
-}
+const urlParams = new URLSearchParams(window.location.search);
+const pointId   = urlParams.get("point");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {isRecorder ? (
-      <MacroRecorder />
-    ) : pointId ? (
+    {pointId ? (
       <PointOverlay id={pointId} />
     ) : (
       <App />
